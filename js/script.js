14a3b32dd1803bcc3740a0c0ae7239800103c8c6
Pablo Coy <pcoy06@gmail.com>
2025-03-02 18:41:29 +0100

    Se agrega proyecto Web Indesa

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const $btnSignIn= document.querySelector('.sign-in-btn'),
       $btnSignUp = document.querySelector('.sign-up-btn'),  
       $signUp = document.querySelector('.sign-up'),
+      
       $signIn  = document.querySelector('.sign-in');
 
 document.addEventListener('click', e => {
